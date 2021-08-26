cbd3e1a2e583e6da07b50a89750b38b969d3df51
Froz10 <ivan.frozby@gmail.com>
2021-08-26 16:38:56 +0600

    Creating spec for app logic

--- spec/lib/distribution_spec.rb
+++ spec/lib/distribution_spec.rb
@@ -4,15 +4,58 @@ require 'rails_helper'
 require './lib/distribution'
 
 RSpec.describe 'Distribution' do
-  it 'return distribution color' do
-    color = Color.new
-    distribution = color.distribution
-    expect(distribution).not_to be_empty
+  let(:arr) { [] }
+  let(:color) { Color.new }
+  let(:price) { Price.new }
+
+  context 'when GET from device' do
+    it 'return distribution color' do
+      distrib = color.distribution
+      expect(distrib).not_to be_empty
+    end
+
+    it 'return distribution price' do
+      distrib = price.distribution
+      expect(distrib).not_to be_empty
+    end
   end
 
-  it 'return distribution price' do
-    price = Price.new
-    distribution = price.distribution
-    expect(distribution).not_to be_empty
+  context 'when GET from device 20 times price' do
+    it 'return distribution probability price 10' do
+      20.times { arr << price.distribution }
+      expect(arr.count('10')).to eq(15)
+    end
+
+    it 'return distribution probability price 20' do
+      20.times { arr << price.distribution }
+      expect(arr.count('20')).to eq(2)
+    end
+
+    it 'return distribution probability price 5' do
+      20.times { arr << price.distribution }
+      expect(arr.count('5')).to eq(2)
+    end
+
+    it 'return distribution probability price 50' do
+      20.times { arr << price.distribution }
+      expect(arr.count('50')).to eq(1)
+    end
+  end
+
+  context 'when GET from device 20 times color' do
+    it 'return distribution probability color #FF0000' do
+      20.times { arr << color.distribution }
+      expect(arr.count('#FF0000')).to eq(7)
+    end
+
+    it 'return distribution probability color #00FF00' do
+      20.times { arr << color.distribution }
+      expect(arr.count('#00FF00')).to eq(7)
+    end
+
+    it 'return distribution probability color #0000FF' do
+      20.times { arr << color.distribution }
+      expect(arr.count('#0000FF')).to eq(6)
+    end
   end
 end
