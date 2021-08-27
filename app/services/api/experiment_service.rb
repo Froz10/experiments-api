cbe0855cadf770c2fedfb62d69d67d1717840505
Alexandr Subbotin <kelionweb@gmail.com>
2021-08-27 13:26:58 +0400

    Refactor 2 iteration

--- app/services/api/experiment_service.rb
+++ app/services/api/experiment_service.rb
@@ -1,41 +1,22 @@
 # frozen_string_literal: true
 
+require 'distribution'
+
 module Api
   class ExperimentService < ApplicationService
+    attr_reader :token, :color_distribution, :price_distribution
+
     def initialize(token) # rubocop:disable Lint/MissingSuper: Call super to initialize state of the parent class.
       @token = token
+      @color_distribution = Distribution::Color.new
+      @price_distribution = Distribution::Price.new
     end
 
     def call
-      default_values
-      attribute_color if query_to_db_valid?
-      attribute_price if query_to_db_valid?
-      experiment_params
-      @experiment = Experiment.create experiment_params
-      @experiments = Experiment.select(:button_color, :price).where(token: @token)
-    end
-
-    private
-
-    def default_values
-      Experiment.colors
-      Experiment.prices
-    end
-
-    def attribute_color
-      @button_color = Experiment.color
-    end
-
-    def attribute_price
-      @price = Experiment.price
-    end
-
-    def query_to_db_valid?
-      !Experiment.exists?(token: @token)
-    end
-
-    def experiment_params
-      @params = { token: @token, button_color: @button_color, price: @price }
+      Experiment.find_or_create_by(token: token) do |experiment|
+        experiment.button_color = color_distribution.next_value
+        experiment.price = price_distribution.next_value
+      end
     end
   end
 end
